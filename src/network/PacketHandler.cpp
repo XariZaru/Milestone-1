d1c4f363614078f613b46742267daf6bd6e86ef5
XariZaru <jonsnguyening@gmail.com>
2017-01-28 21:05:03 -0800

    - PacketHandler

---
+++ src/network/PacketHandler.cpp
@@ -0,0 +1,18 @@
+/*
+ * PacketHandler.cpp
+ *
+ *  Created on: Jan 28, 2017
+ *      Author: Jon
+ */
+
+#include "PacketHandler.h"
+
+PacketHandler::PacketHandler() {
+	// TODO Auto-generated constructor stub
+
+}
+
+PacketHandler::~PacketHandler() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/network/PacketHandler.h
@@ -0,0 +1,17 @@
+/*
+ * PacketHandler.h
+ *
+ *  Created on: Jan 28, 2017
+ *      Author: Jon
+ */
+
+#ifndef PACKETHANDLER_H_
+#define PACKETHANDLER_H_
+
+class PacketHandler {
+public:
+	PacketHandler();
+	virtual ~PacketHandler();
+};
+
+#endif /* PACKETHANDLER_H_ */
