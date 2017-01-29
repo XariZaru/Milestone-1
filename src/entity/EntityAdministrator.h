1d72aa6a31d68ad11c7d09637fafbb307083be80
XariZaru <jonsnguyening@gmail.com>
2017-01-28 21:04:53 -0800

    - EntityAdministrator

---
+++ src/entity/EntityAdministrator.h
@@ -0,0 +1,19 @@
+/*
+ * EntityAdministrator.h
+ *
+ *  Created on: Jan 28, 2017
+ *      Author: Jon
+ */
+
+#ifndef ENTITYADMINISTRATOR_H_
+#define ENTITYADMINISTRATOR_H_
+
+
+class EntityAdministrator {
+public:
+	EntityAdministrator();
+	virtual ~EntityAdministrator();
+};
+
+
+#endif /* ENTITYADMINISTRATOR_H_ */
