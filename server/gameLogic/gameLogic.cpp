df2324ee426f91a1c637cee8b22a07db10a4c9ba
Alexander Lee <al76267@gmail.com>
2017-01-28 21:13:40 -0800

    Initial implementation of game logic
    - basic update functions
    - currently keeps track of one snake

---
+++ server/gameLogic/gameLogic.cpp
@@ -0,0 +1,60 @@
+#include "gameLogic.hpp"
+#include <random>
+
+std::pair<int, int> getDirection(std::pair<int, int> x1, std::pair<int, int> x2)
+{
+    return std::pair<int, int> {x2.first - x1.first, x2.second - x1.second};
+}
+
+SnakeGame::SnakeGame()
+: head(randomLoc()), direction(std::pair<int, int> {0, 1})
+{
+    if (head.second + snakeLength >= boardSize)
+        head.second -= snakeLength;
+    tail.first = head.first; tail.second = head.second + snakeLength;
+}
+
+std::pair<int, int> SnakeGame::getTail()
+{
+    return snake.back();
+}
+
+std::pair<int, int> SnakeGame::randomLoc()
+{
+    std::mt19937 rng;
+    rng.seed(std::random_device()());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(0, boardSize-1);
+    return std::pair<int, int>{ dist(rng), dist(rng) };
+}
+    
+void SnakeGame::setBoardSize(int size)
+{
+    boardSize = size;
+}
+
+void SnakeGame::elongate()
+{
+    head.first += direction.first; head.second += direction.second;
+    snakeLength++;
+}
+
+// Updates direction based on user input.
+void SnakeGame::updateDirection(std::pair<int, int> newDirection)
+{
+    direction = newDirection;
+}
+
+// Updates the head and tail of the snake accordingly.
+// Removes the locations of turns for the snake when the tail
+// is at the first element of the snake turn location queue.
+void SnakeGame::updateSnake()
+{
+    std::pair<int, int> tailDirection, firstTurn = snake.front();
+    head.first += direction.first; head.second += direction.second;
+    direction = getDirection(head, firstTurn);
+    tail.first += tailDirection.first; tail.second += tailDirection.second;
+    if (tail == firstTurn)
+        snake.pop();
+}
+
+
---
+++ server/gameLogic/gameLogic.hpp
@@ -0,0 +1,33 @@
+#ifndef gameLogic_hpp
+#define gameLogic_hpp
+
+#include <stdio.h>
+#include <queue>
+#include <map>
+
+std::pair<int, int> getDirection(std::pair<int, int> x1, std::pair<int, int> x2);
+
+class SnakeGame
+{
+public:
+    SnakeGame();
+    ~SnakeGame();
+    
+    std::pair<int, int> getTail();
+    std::pair<int, int> randomLoc();
+    void elongate();
+    void updateDirection(std::pair<int, int> newDirection);
+    void updateSnake();
+    
+    void setBoardSize(int size);
+    
+private:
+    
+    std::queue<std::pair<int, int>> snake;
+    int snakeLength = 4;
+    int boardSize = 30; // square board
+    std::pair<int, int> head, tail, direction;
+    std::map<int, std::pair<int, int>> directions;
+};
+
+#endif /* gameLogic_hpp */
