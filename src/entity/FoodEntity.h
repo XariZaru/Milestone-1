6b54c6d5e753889294febfae694f049c53b526b6
Wesley <bionicle_1121@hotmail.com>
2017-02-16 12:54:56 -0800

    Change Source to Header
    Moved code into FoodEntity header file and deleted source file.

---
+++ src/entity/FoodEntity.h
@@ -0,0 +1,27 @@
+/*
+* FoodEntity.h
+*
+*  Created on: Feb 16, 2017
+*      Author: Wesley
+*/
+
+#ifndef FOODENTITY_H_
+#define FOODENTITY_H_
+
+#include "../objects/GameEntity.h"
+
+#include <map>
+
+class FoodEntity : public GameEntity {
+	FoodEntity();
+	virtual ~FoodEntity();
+
+	std::pair<int, int> position();
+	std::pair<int, int> respawn();
+
+private:
+	std::pair<int, int> x_loc, y_loc;
+
+};
+
+#endif /* FOODENTITY_H_ */
